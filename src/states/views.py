a66d7bdaa188bb665e8c03a109c7279447712db6
qwergram <npengra317@gmail.com>
2018-11-29 15:58:34 -0800

    add old maps to selections

--- src/states/views.py
+++ src/states/views.py
@@ -13,6 +13,7 @@ class NewMapView(TemplateView):
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
         context['states'] = State.objects.all()
+        context['maps'] = SeedRedistrictMap.objects.all()
         return context
 
     def post(self, request, *args, **kwargs):
