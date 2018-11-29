9e4cfe00bc2069085cc3439db3d3d9266d160f5f
qwergram <npengra317@gmail.com>
2018-11-29 15:51:14 -0800

    import error

--- src/wxml/urls.py
+++ src/wxml/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
-from states.views import NewMapView
+from states.views import NewMapView, ExistingMapsView
 from django.conf.urls.static import static
 
 urlpatterns = [
