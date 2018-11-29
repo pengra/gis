5e4bca48bc40e978a3173b49e1fc17c6b25c3a80
Norton Pengra <npengra317@gmail.com>
2018-11-29 11:02:43 -0800

    typos

--- wxml/wxml/celery.py
+++ wxml/wxml/celery.py
@@ -1,5 +1,5 @@
 import os
-from celery import celery
+from celery import Celery
 from wxml import settings
 
 app = Celery('wxml')
@@ -7,6 +7,6 @@ app = Celery('wxml')
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
-@app.taks(bind=True)
+@app.task(bind=True)
 def debug_task(self):
     print("Request: {0!r".format(self.request))
