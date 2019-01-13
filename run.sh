dda327b62c2dd5bb52513e6b660bdc7aa60199a1
Norton Pengra <npengra317@gmail.com>
2019-01-13 06:57:54 +0000

    migrations and upgrade to python 3.7.2

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ cd $SERVER_LOCATION
 . ".env/bin/activate"
 cd "src"
 python manage.py collectstatic --no-input
-gunicorn --workers=2 -b 127.0.0.1:9996 wxml.wsgi:application
+gunicorn --workers=1 -b 127.0.0.1:9996 wxml.wsgi:application
