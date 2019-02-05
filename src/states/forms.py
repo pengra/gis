f96619d22005f21ec3035a559f7fc0d37844c75c
qwergram <npengra317@gmail.com>
2019-02-05 09:22:39 -0800

    define API forms

---
+++ src/states/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+class InitialForm(forms.Form):
+    code = forms.CharField()
+    mode = forms.CharField()
+
+class CreateRunForm(forms.Form):
+    state = forms.IntegerField() # fips code
+    districts = models.IntegerField() # num of districts
+
+class BulkEventPushForm(forms.Form):
+    run = forms.UUIDField() # event id
+    file = forms.FileField() # events.pk3
