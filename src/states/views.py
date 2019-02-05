985dcc2f5493d2677e41e02ec0e00c39751da29e
qwergram <npengra317@gmail.com>
2019-02-05 09:23:25 -0800

    add dummy API that uses forms

--- src/states/views.py
+++ src/states/views.py
@@ -4,6 +4,7 @@ from django.http import JsonResponse
 from states.models import Event, Run
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
+from states.forms import InitialForm, CreateRunForm, BulkEventPushForm
 
 # from state.models import Run
 
@@ -24,11 +25,16 @@ class StateListView(TemplateView):
 class APIView(TemplateView):
     template_name = "home/api.html"
 
-    def post(self, *args, **kwargs):
-        return JsonResponse(self.json(*args, **kwargs))
+    def post(self, request, *args, **kwargs):
+        initial = InitialForm(request.POST)
+        if initial.is_valid() and initial.cleaned_data['code'] == 'default_code': # TODO: Change this to os.getenv
+            return JsonResponse(self.json(*args, **kwargs))
+        return JsonResponse({"error": True, "message": "Unknown Operation/Invalid Code"}, status_code=400)
 
-    def json(self, *args, **kwargs):
+    def json(self):
         return {
+            "error": False,
+            "message": "success",
             "args": str(args),
             "kwargs": str(kwargs)
         }
