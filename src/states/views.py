20c0b5203e3a9f5886ce7febfcfc9c992b94b00b
qwergram <npengra317@gmail.com>
2019-02-28 12:17:13 -0800

    reduce query

--- src/states/views.py
+++ src/states/views.py
@@ -22,7 +22,7 @@ class DataView(TemplateView):
         context = super().get_context_data(*args, **kwargs)
         context['runs'] = Run.objects.all()
         context['tasks'] = ProcessQueue.objects.filter(
-            status='queued').count() + 1
+            status='queued').limit(100).count() + 1
         context['working'] = ProcessQueue.objects.filter(status='running')
         return context
 
